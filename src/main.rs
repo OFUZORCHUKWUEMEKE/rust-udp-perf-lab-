ffc5c0928ea613faa5a307b9f2dcd4d585839971
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2026-06-04 00:55:38 +0100

    telemetrics

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,34 @@ use std::collections::HashMap;
 use std::net::UdpSocket;
 
 fn main() -> std::io::Result<()> {
-    let socket = UdpSocket::bind("0.0.0.0:9000")?;
+    let socket = UdpSocket::bind("0.0.0.0:7000")?;
 
     let mut total_packets = 0u64;
     let mut source_counts = HashMap::new();
+
+    let mut total_bytes = 0u64;
+    let mut max_size = 0usize;
+    let mut min_size = usize::MAX;
 
     println!("Listening on {}", socket.local_addr()?);
 
     let mut buffer = [0u8; 2048];
 
     loop {
-        total_packets += 1;
+        let (bytes_recieved, src_addr) = socket.recv_from(&mut buffer)?;
 
-        let (_bytes_recieved, src_addr) = socket.recv_from(&mut buffer)?;
+        total_packets += 1;
+        total_bytes += bytes_recieved as u64;
+
+        max_size = max_size.max(bytes_recieved);
+        min_size = min_size.min(bytes_recieved);
+
+        let avg_size = total_bytes as f64 / total_packets as f64;
         *source_counts.entry(src_addr.ip()).or_insert(0) += 1;
-        println!("Total packets : {}", total_packets);
-        println!("Unique sources : {}", source_counts.len());
+
+        println!(
+            "Packet from {} | size={} | avg={:.2}",
+            src_addr, bytes_recieved, avg_size
+        );
     }
 }
